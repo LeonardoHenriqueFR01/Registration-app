3e467d0231c97113bb4ac075de972463c10e091d
Leozindev_genius <leonardohenriquefr785@gmail.com>
2025-05-13 19:52:54 -0300

    removendo /uploads

--- app/static/js/new_product.js
+++ app/static/js/new_product.js
@@ -1,39 +1,3 @@
-/*
-
-const video = document.getElementById('video');
-const canvas = document.getElementById('canvas');
-const imageDataInput = document.getElementById('image_data');
-
-// Solicita acesso à câmera (traseira em celulares)
-navigator.mediaDevices.getUserMedia({
-    video: {
-        facingMode: { exact: "environment" }  // Tenta a câmera traseira
-    }
-}).then(stream => {
-    video.srcObject = stream;
-}).catch(err => {
-    console.warn("Câmera traseira não disponível. Tentando câmera padrão...");
-    // Tenta qualquer câmera disponível
-    return navigator.mediaDevices.getUserMedia({ video: true });
-}).then(stream => {
-    if (stream) video.srcObject = stream;
-}).catch(err => {
-    console.error("Erro ao acessar a câmera:", err);
-    alert("Erro ao acessar a câmera. Verifique as permissões.");
-});
-
-function tirarFoto() {
-    const context = canvas.getContext('2d');
-    context.drawImage(video, 0, 0, canvas.width, canvas.height);
-    const dataUrl = canvas.toDataURL('image/png');
-    imageDataInput.value = dataUrl;
-    canvas.style.display = 'none';  // Mostra o canvas com a imagem capturada
-
-    alert('Foto capturada!')
-}
-
-*/
-
 const video = document.getElementById('video');
 const canvas = document.getElementById('canvas');
 const imageDataInput = document.getElementById('image_data');
